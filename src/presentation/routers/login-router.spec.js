18238ab2cdf1be39945b072f1b518133e6f13d80
pablo_santos <pablo.1999@alunos.utfpr.edu.br>
2022-04-10 19:32:18 -0300

    ensure LoginRouter return 401 when invalid credentials ate provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -12,7 +12,8 @@ const makeSut = () => {
   const authUseCaseSpy = new AuthUseCaseSpy()
   const sut = new LoginRouter(authUseCaseSpy)
   return {
-    sut, authUseCaseSpy
+    sut,
+    authUseCaseSpy
   }
 }
 
@@ -66,5 +67,18 @@ describe('Login Router', () => {
     sut.route(httpRequest)
     expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
     expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
+  })
+
+  test('should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@email.com',
+        password: 'invalid_password'
+      }
+    }
+
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
   })
 })
